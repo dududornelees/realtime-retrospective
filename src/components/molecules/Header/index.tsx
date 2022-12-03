af3916fb361b2604596a8766aac7d7520cf9a3a8
dududornelees <eduardo.dsdorneles@gmail.com>
2022-12-02 23:24:08 -0300

    feat: criando estrutura atomic

---
+++ src/components/molecules/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="h-16 bg-primary flex items-center justify-center">
+            <h1 className="text-white text-xl font-semibold">Retrospectiva</h1>
+        </header>
+    );
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: "#94248a",
+                primaryDark: "#821f79",
+                secondary: "#004c91",
+                secondaryDark: "#003d75"
+            }
+        }
     },
     plugins: []
 };
